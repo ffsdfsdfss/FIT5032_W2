284052182cb76310fc4c33ab6dbb3f20a6f6540b
ffsdfsdfss <1961464063@qq.com>
2024-07-18 20:59:43 +0800

    Initial commit with C# project files

---
+++ FIT5032_MyCodeSnippet/FIT5032_MyCodeSnippet/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using FIT5032_Week03.HelloWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FIT5032_MyCodeSnippet.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            // 注释掉原有代码
+            // ViewBag.Message = "Your application description page.";
+
+            // 创建Hello对象的实例
+            Hello hello = new Hello();
+
+            // 调用GetHello方法，并将结果赋值给ViewBag.Message
+            ViewBag.Message = hello.GetHello();
+
+            return View();
+        }
+
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
